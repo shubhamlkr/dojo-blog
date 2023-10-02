8936a30cb4e2f4b6084681429e3381a6f9e5a8e7
shubhamlkr <shubhamkr78700@gmail.com>
2023-10-02 18:53:03 +0530

    hardcoded data

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,31 +1,45 @@
 import { useHistory, useParams } from "react-router-dom";
-import useFetch from "./useFetch";
+// import useFetch from "./useFetch";
+import { useState } from "react";
+import blogs from "./data/db.json"
 
 const BlogDetails = () => {
-  const { id } = useParams();
-  const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
-  const history = useHistory();
+  // const { id } = useParams();
+  // const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+  // const history = useHistory();
 
   const handleClick = () => {
-    fetch('http://localhost:8000/blogs/' + blog.id, {
-      method: 'DELETE'
-    }).then(() => {
-      history.push('/');
-    }) 
+    // fetch('http://localhost:8000/blogs/' + blog.id, {
+    //   method: 'DELETE'
+    // }).then(() => {
+    //   history.push('/');
+    // }) 
+    alert("Server internal error")
   }
+  const [Blog,setBlog]=useState(blogs.blogs)
 
   return (
     <div className="blog-details">
-      { isPending && <div>Loading...</div> }
-      { error && <div>{ error }</div> }
-      { blog && (
+      {/* { isPending && <div>Loading...</div> } */}
+      {/* { error && <div>{ error }</div> } */}
+      {/* { blog && (
         <article>
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
           <div>{ blog.body }</div>
           <button onClick={handleClick}>delete</button>
         </article>
-      )}
+      )} */}
+      {
+        Blog.map((b)=>(
+                <article>
+                <h2>{ b.title }</h2>
+                <p>Written by { b.author }</p>
+                <div>{ b.body }</div>
+                <button onClick={handleClick}>delete</button>
+                </article>
+              ))
+      }
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,17 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import Bloglist from './Bloglist';
 import useFetch from './useFetch';
+import blogs from "./data/db.json"
 const Home= () => {
-    const {data:blogs,ispending,error}=useFetch(' http://localhost:8000/blogs');
-    
+    // const {data:blogs,ispending,error}=useFetch(' http://localhost:8000/blogs');
+    const [Blog, setBlog]=useState(blogs)
     return (  
         <div className="home">
-        {error && <div>{error}</div> }
-        {ispending && <div>loading..</div> }
-        {blogs && <Bloglist blogs={blogs} title="All blogs"  />}
+        {/* {error && <div>{error}</div> }
+        {ispending && <div>loading..</div> } */}
+        {/* {blog && <Bloglist blogs={blog} title="All blogs"  />} */}
+        {Blog && <Bloglist blogs={Blog.blogs} title="All blogs"  />}
         </div>
         
     );
